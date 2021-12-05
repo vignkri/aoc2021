402a0b1ad9deab27fec0658722689a8e8fc5fccc
Vignesh Krishnamoorthy <vignkri@gmail.com>
2021-12-05 15:52:43 +0100

    compute day-2-1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
+use std::path::{Path, self};
+
+use argparse::action;
 
 
 pub mod day_one;
-
+pub mod day_two;
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
@@ -30,5 +32,34 @@ fn main() {
 
     println!("DAY 1.1 : Count of increases: {}", day_one_1_solution);
     println!("DAY 1.2 : Count of sum increases: {}", day_one_2_solution);
+
+    let path_to_file = "./data/day2.txt";
+
+    // parsed dataset
+    let dataset = day_two::parser(read_lines(path_to_file));
+
+    // Initialise a submarine
+    let mut submarine = day_two::Submarine::new();
+
+    for row in dataset {
+
+        let action_magnitude: Vec<&str> = row.split(" ").collect();
+
+        let action = action_magnitude[0];
+        let magnitude: i64 = action_magnitude[1].parse::<i64>().unwrap();
+
+        if action == "forward" {
+            submarine = submarine.move_forward(magnitude);
+        } else if action == "up" {
+            submarine = submarine.move_up(magnitude);
+        } else if action == "down" {
+            submarine = submarine.move_down(magnitude);
+        }
+        println!("After every action: {:?}", submarine);
+    }
+    
+    println!("Day two dataset: {:?}", submarine.travelled_units());
+
+
 
 }
